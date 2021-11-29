eb9fda0de78f5aab4501663d8e6623488d86307c
Desind <zaran1998@gmail.com>
2021-11-29 16:21:43 +0100

    fix

---
+++ src/components/recipeCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function RecipeCard(props){
+    return(
+        <div className={styles.inputWrapper}>
+            asdf
+        </div>
+    )
+}
